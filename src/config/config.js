6cf9915dd28ad78d3262207209f5bbf950c79f6d
antonpopov <aopopov90@gmail.com>
2023-12-03 13:10:21 +0000

    ext api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? window.env.API_URL : "http://localhost:8080";
+// export const API_URL = process.env.NODE_ENV === 'production' ? window.env.API_URL : "http://localhost:8080";
+export const API_URL = window._env_.API_URL;
