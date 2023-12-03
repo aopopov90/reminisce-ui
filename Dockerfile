37e29bc7b58b69868dd12117c15c0b0da24a78e1
antonpopov <aopopov90@gmail.com>
2023-12-03 12:01:52 +0000

    ext api url

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM nginx:alpine
 COPY /build/ /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 8080
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["/bin/sh", "-c", "/usr/share/nginx/html/env.sh && nginx -g \"daemon off;\""]
--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,1 @@
-console.log("REACT_APP_API_URL: " + process.env.REACT_APP_API_URL)
-export const API_URL = process.env.REACT_APP_API_URL ?? 'http://localhost:8080';
+export const API_URL = process.env.NODE_ENV === 'production' ? window.env.API_URL : process.env.REACT_APP_API_URL;
